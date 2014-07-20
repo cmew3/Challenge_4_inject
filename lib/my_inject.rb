56554ddabc539e74ed1cbf3f6f558e4992d01d51
cmew3 <c.wheeler-booth@cantab.net>
2014-07-20 19:39:15 +0100

    wrote method for my_inject

---
+++ lib/my_inject.rb
@@ -0,0 +1,25 @@
+class NewArray < Array
+
+	def initialize(*args)
+		args.each do |arg|
+			self << arg
+		end
+	end
+
+
+	def my_inject start = :default_value, &block
+		if start == :default_value
+			result = self[0]
+			block_range = (1..(self.length-1))
+		else
+			result = start
+			block_range = (0..(self.length-1))
+		end	
+		for i in block_range
+			result = block.call(result,self[i])
+		end
+		result
+	end
+
+
+end	
---
+++ spec/my_inject_spec.rb
@@ -0,0 +1,70 @@
+require 'my_inject'
+
+describe NewArray do
+
+	let(:newarray) {NewArray.new(1,2,3,4)}
+	let(:wordarray) {NewArray.new("may","i","have","a","large","container","of","coffee")}
+
+	add_up = Proc.new do |x,y|
+		x+y
+	end
+
+	# product = Proc.new do |x,y|
+	# 	x*y
+	# end
+
+	it 'is a subclass of array' do
+		newarray = NewArray.new
+		expect(newarray).to be_a(Array)
+	end
+
+	context 'has a my_inject method that' do
+		
+		add_up = Proc.new do |x,y|
+			x+y
+		end
+
+		product = Proc.new do |x,y|
+			x*y
+		end
+
+		longest_word = Proc.new do |memo,word|
+			memo.length > word.length ? memo : word
+		end
+
+		context 'when no start value is given' do
+
+			it 'can add an array' do
+				expect(newarray.my_inject(&add_up)).to eq newarray.inject(&add_up)
+			end	
+
+			it 'can give the product of an array' do
+				expect(newarray.my_inject(&product)).to eq newarray.inject(&product)
+			end
+
+			it 'can give the longest word in an array' do
+				expect(wordarray.my_inject(&longest_word)).to eq wordarray.inject(&longest_word)
+			end
+
+		end
+
+		context 'when no start value is given' do
+
+			it 'can add an array' do
+				expect(newarray.my_inject(10,&add_up)).to eq newarray.inject(10,&add_up)
+			end	
+
+			it 'can give the product of an array' do
+				expect(newarray.my_inject(10,&product)).to eq newarray.inject(10,&product)
+			end
+
+			it 'can give the longest word in an array' do
+				expect(wordarray.my_inject("habidasher",&longest_word)).to eq wordarray.inject("habidasher",&longest_word)
+			end
+
+
+		end
+
+
+	end	
+end	
